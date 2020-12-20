3d8c072db9d9621f3f72fc246ec8a70da09954fd
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2020-12-20 21:43:55 +0600

    Secure Sub Folder access from outside - 154.2

---
+++ admin/upload/index.php
@@ -0,0 +1,15 @@
+<?php 
+    // header("location: ../index.php");
+    // This also work, location is different for various file
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>File Not Found.</h1>
+</body>
+</html>
